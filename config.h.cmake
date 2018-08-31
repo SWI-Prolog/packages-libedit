05cc4f50e99dcba7df03e947b1280cf38618c748
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-08-31 11:54:31 +0200

    CMAKE: First migration of a package to cmake

---
+++ config.h.cmake
@@ -0,0 +1,9 @@
+#cmakedefine HAVE_FCNTL_H
+#cmakedefine HAVE_POLL_H
+#cmakedefine HAVE_SYS_IOCTL_H
+#cmakedefine HAVE_SYS_SELECT_H
+#cmakedefine HAVE_UNISTD_H
+
+#cmakedefine HAVE_EL_CURSOR
+#cmakedefine HAVE_EL_WSET
+#cmakedefine HAVE_POLL
